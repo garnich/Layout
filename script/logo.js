398ea4810f9feccbef2946e4e2a4177d69a052bb
garnich <garnich@tut.by>
2019-06-16 17:54:07 +0300

    feat: add animation

--- script/logo.js
+++
@@ -1,7 +0,0 @@
-const logo = document.querySelectorAll('#myLogo path');
-
-console.log(logo);
-// logo.map((item, index) => console.log(`Letter ${index} is ${item.getTotalLength()}`));
-for(let i=0; i<logo.length; i++){
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
-}
